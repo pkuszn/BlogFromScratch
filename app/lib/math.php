1b294983e94847879ffdcf61f834be65d57ab9cc
patryk <patryk.kuszneruk1798@gmail.com>
2022-01-05 19:42:56 +0100

    Finished changing the structure of the page | Moved blackjack game to views | Resolved  problem with pagination

---
+++ app/lib/math.php
@@ -0,0 +1,26 @@
+<?php
+if(isset($_POST['action']) == 'math' AND $_REQUEST['A'] = 'A' AND $_REQUEST['B'] = 'B' AND $_REQUEST['C'] = 'C'){
+    quadraticEquation($_REQUEST['A'],$_REQUEST['B'],$_REQUEST['C']);
+}
+
+function quadraticEquation($A, $B, $C){
+
+    // a*x^2 + b * x + c
+    $x1 = 0;
+    $x2 = 0;
+    $delta = ($B * $B) - 4 * $A * $C;
+
+    if($delta < 0) {
+        echo "No answer";
+    }
+    else if($delta == 0){
+        $x1 = -$B/(2 * $A);
+        echo "The equation has one solution: " . $x1;
+    }
+    else if($delta > 0){
+        $x1 = (-$B -(sqrt($delta)))/(2*$A);
+        $x2 = (-$B +(sqrt($delta)))/(2*$A);
+        echo "The equation has two solutions: " . "x1: " . $x1 . " x2: " . $x2;
+    }
+}
+?>
--- app/lib/pagination.php
+++ app/lib/pagination.php
@@ -38,4 +38,6 @@ function Offset($pageNow, $perPage){
     return $offset;
 }
 
+
+
 ?>
